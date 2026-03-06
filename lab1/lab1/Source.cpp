596f3d1289e873915cfaa0c5c696b9f669009cf3
puiuld <puiuld47@gmail.com>
2026-03-06 12:40:53 +0200

    Add files via upload

---
+++ lab1/lab1/Source.cpp
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+int main() 
+{
+	/*double x = 2;        //1
+	double x1, x2, x3;
+	double result;
+	x1 = pow(x, 4);
+	x2 = pow(x, 3);
+	x3 = pow(x, 2);
+	result = 2 * x1 - 3 * x2 + 4 * x3 - 5*x + 6;
+	printf("Result:%lf\n", result);
+	return 0;*/
+
+
+
+	/*int n = 1245;     //6
+	int hours = n / 3600;
+	int remaining_seconds = n % 3600;
+	int minutes = remaining_seconds / 60;
+	printf("full hours %d and %d full minutes have passed.\n", hours, minutes);
+	return 0;*/
+
+
+
+	/*double x = 2;     //11
+	double x4, x12, x28;
+	x4 = x * x * x * x;
+	x12 = x4 * x4 * x4;
+	x28 = x12 * x12 * x4;
+	printf("Enter numbers:\n%.0lf\n%.0lf\n%.0lf", x4, x12, x28);
+	return 0;*/
+}
